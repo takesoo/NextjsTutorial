c44624639e1061f1dfa9468c2a8372fedd1bdc5a
takesoo <takesooon@gmail.com>
2022-03-20 18:16:53 +0900

    study Script component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
@@ -8,6 +9,13 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log(`script loaded correctly, window.FB has been populated`);
+        }}
+      />
       <h1>First Post</h1>
       <h2>
         <Link href={"/"}>
