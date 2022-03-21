c10233babf9d6a4a52af298a6bca91c1b9c80177
takesoo <takesooon@gmail.com>
2022-03-21 12:04:01 +0900

    add error handling

--- components/user.js
+++ components/user.js
@@ -7,15 +7,24 @@ import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 
 const fetchUsers = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.co");
   return res.json();
 };
 
 export const User = () => {
-  /* useQuery('ユニークキー', 'Promiseを返す関数') */
-  const { data, isLoading } = useQuery("users", fetchUsers);
+  /**
+   * isErrorとerrorでエラー内容を表示する
+   * 第三引数でリトライ回数などを設定できる
+   */
+
+  const { data, isLoading, isError, error } = useQuery("users", fetchUsers, {
+    retry: false,
+  });
   if (isLoading) {
     return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Error: {error.message}</span>;
   }
   return (
     <div>
