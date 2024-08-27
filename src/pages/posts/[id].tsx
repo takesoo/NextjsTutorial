4b559d60487ef6b8e9404f26f089fdf24e6ac3f6
takesoo <takesooon@gmail.com>
2024-08-27 09:36:43 +0900

    pages router routing

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router"
+
+export default function Post() {
+  const router = useRouter();
+  const {id} = router.query;
+  return (
+    <div>
+      <h1>Post {id}</h1>
+      <p>This is the content of post {id}</p>
+    </div>
+  )
+}
