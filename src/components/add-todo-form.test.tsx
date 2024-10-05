e600ea61f874f1eef78f0d30dde62387ecabc8d8
takesoo <takesooon@gmail.com>
2024-10-05 15:49:54 +0900

    add snapshot test

--- src/components/add-todo-form.test.tsx
+++ src/components/add-todo-form.test.tsx
@@ -1,46 +1,59 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { AddTodoForm } from "./add-todo-form";
 
-describe("AddTodoForm", () => {
-  const mockSetTodoTitle = jest.fn();
-  const mockAddTodo = jest.fn();
-  it("renders input and button", () => {
-    render(
+// describe("AddTodoForm", () => {
+//   const mockSetTodoTitle = jest.fn();
+//   const mockAddTodo = jest.fn();
+//   it("renders input and button", () => {
+//     render(
+//       <AddTodoForm
+//         newTodoTitle=""
+//         setNewTodoTitle={mockSetTodoTitle}
+//         addTodo={mockAddTodo}
+//       />,
+//     );
+//     expect(screen.getByPlaceholderText("New Todo")).toBeInTheDocument();
+//     expect(
+//       screen.getByRole("button", { name: /add todo/i }),
+//     ).toBeInTheDocument();
+//   });
+
+//   it("calls addTodo when the button is clicked", () => {
+//     render(
+//       <AddTodoForm
+//         newTodoTitle="Test Todo"
+//         setNewTodoTitle={mockSetTodoTitle}
+//         addTodo={mockAddTodo}
+//       />,
+//     );
+//     const button = screen.getByRole("button", { name: /add todo/i });
+//     fireEvent.click(button);
+//     expect(mockAddTodo).toHaveBeenCalled();
+//   });
+
+//   it("calls setNewTodoTitle on input change", () => {
+//     render(
+//       <AddTodoForm
+//         newTodoTitle=""
+//         setNewTodoTitle={mockSetTodoTitle}
+//         addTodo={mockAddTodo}
+//       />,
+//     );
+//     const input = screen.getByPlaceholderText("New Todo");
+//     fireEvent.change(input, { target: { value: "New Task" } });
+//     expect(mockSetTodoTitle).toHaveBeenCalledWith("New Task");
+//   });
+// });
+
+describe("AddTodoForm component", () => {
+  it("matches snapshot", () => {
+    const { container } = render(
       <AddTodoForm
         newTodoTitle=""
-        setNewTodoTitle={mockSetTodoTitle}
-        addTodo={mockAddTodo}
+        setNewTodoTitle={jest.fn()}
+        addTodo={jest.fn()}
       />,
     );
-    expect(screen.getByPlaceholderText("New Todo")).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: /add todo/i }),
-    ).toBeInTheDocument();
-  });
-
-  it("calls addTodo when the button is clicked", () => {
-    render(
-      <AddTodoForm
-        newTodoTitle="Test Todo"
-        setNewTodoTitle={mockSetTodoTitle}
-        addTodo={mockAddTodo}
-      />,
-    );
-    const button = screen.getByRole("button", { name: /add todo/i });
-    fireEvent.click(button);
-    expect(mockAddTodo).toHaveBeenCalled();
-  });
-
-  it("calls setNewTodoTitle on input change", () => {
-    render(
-      <AddTodoForm
-        newTodoTitle=""
-        setNewTodoTitle={mockSetTodoTitle}
-        addTodo={mockAddTodo}
-      />,
-    );
-    const input = screen.getByPlaceholderText("New Todo");
-    fireEvent.change(input, { target: { value: "New Task" } });
-    expect(mockSetTodoTitle).toHaveBeenCalledWith("New Task");
+    expect(container).toMatchSnapshot();
   });
 });
